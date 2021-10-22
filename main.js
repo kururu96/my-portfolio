a1c14967e7a247a51f377a1be99d5190e36df16e
kururu96 <5789atom@gmil.com>
2021-10-22 20:59:41 +0900

    ポートフォリオ

---
+++ main.js
@@ -0,0 +1,12 @@
+const Button = document.getElementById("button");
+const Button1 = document.getElementById("button1");
+const cake = document.getElementById("cake");
+const display = document.getElementById("display");
+
+// 各ボタン要素がクリックされた時の処理を指定
+Button1.addEventListener("click", function () {
+  cake.style.opacity = 0; // 画像要素のopacityプロパティの値を0(透明)にする*/
+});
+Button.addEventListener("click", function () {
+  cake.style.opacity = 1; // 画像要素のopacityプロパティの値を1(不透明)にする*/
+});
